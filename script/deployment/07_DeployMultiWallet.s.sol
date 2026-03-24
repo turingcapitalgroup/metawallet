50d27d06009ec4849e988ffbb316ee496c146d3f
addressZero <filipeepv@gmail.com>
2026-03-24 12:03:00 +0000

    admin not owner

--- script/deployment/07_DeployMultiWallet.s.sol
+++ script/deployment/07_DeployMultiWallet.s.sol
@@ -198,7 +198,7 @@ contract DeployMultiWalletScript is Script, DeploymentManager {
         MetaWallet metaWallet = MetaWallet(payable(wallet.proxy));
 
         // Grant ADMIN_ROLE to owner
-        metaWallet.grantRoles(config.roles.owner, 1); // ADMIN_ROLE = 1
+        metaWallet.grantRoles(config.roles.admin, 1); // ADMIN_ROLE = 1
         _log("ADMIN_ROLE granted to deployer");
 
         // Setup VaultModule
